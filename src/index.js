febe1c765c43eabf765ff057ad73a45f0623b254
Aaron Faber <Aaron.Faber@digitaslbi.com>
2014-10-16 15:48:30 +0100

    Refactored code to work with seneca

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
-var errorDictionary = require('./error_dictionary');
-var phraseDictionary = require('./phrase_dictionary');
+var errorDictionary = require('./error_dictionary')
+var phraseDictionary = require('./phrase_dictionary')
 
-module.exports = function createRecipeFrom (ingredients) {
-    if(!(ingredients instanceof Array)) throw new Error(errorDictionary.NOT_AN_ARRAY);
-    if (ingredients.length > 2) throw new Error(errorDictionary.MORE_THAN_TWO_INGREDIENTS);
-    if (ingredients.length < 2) throw new Error(errorDictionary.LESS_THAN_TWO_INGREDIENTS);
+module.exports = function recipe () {
+    var seneca = this;
 
-    var recipeString = '';
+    seneca.add({role: 'recipe', cmd: 'generate'}, function (args, callback) {
+        var ingredients = args.ingredients;
 
-    recipeString += phraseDictionary.firstPhrase;
-    recipeString += ' ';
-    recipeString += ingredients.join(' and ');
-    recipeString += ' ';
-    recipeString += addRandomPhraseFrom(phraseDictionary.phrases);
-    recipeString += phraseDictionary.lastPhrase;
+        if(!(ingredients instanceof Array)) callback(errorDictionary.NOT_AN_ARRAY, null)
+        if (ingredients.length > 2) callback(errorDictionary.MORE_THAN_TWO_INGREDIENTS, null)
+        if (ingredients.length < 2) callback(errorDictionary.LESS_THAN_TWO_INGREDIENTS, null)
 
-    return recipeString;
-};
+        var recipeString = ''
+
+        recipeString += phraseDictionary.firstPhrase
+        recipeString += ' '
+        recipeString += ingredients.join(' and ')
+        recipeString += ' '
+        recipeString += addRandomPhraseFrom(phraseDictionary.phrases)
+        recipeString += phraseDictionary.lastPhrase
+
+        callback(null, recipeString)
+    })
+}
 
 function addRandomPhraseFrom(phraseDictionary) {
     return phraseDictionary[Math.floor(Math.random()*phraseDictionary.length)]
--- test/test.js
+++ test/test.js
@@ -1,60 +1,79 @@
-var expect = require('chai').expect;
-var errorDictionary = require('../src/error_dictionary');
-var phraseDictionary = require('../src/phrase_dictionary');
-var createRecipeFrom = require('../src/index');
+var expect = require('chai').expect
+var seneca = require('seneca')
+var errorDictionary = require('../src/error_dictionary')
+var phraseDictionary = require('../src/phrase_dictionary')
 
 describe('recipe generator module', function () {
+    var si;
+
+    before(function () {
+        si = seneca({log:'silent'})
+                .use('../src/index')
+    })
+
     describe('when I have inputted more than two ingredients', function () {
-        var ingredients = ['apples', 'bananas', 'pears'];
+        var ingredients = ['apples', 'bananas', 'pears']
 
         it('should return an error', function () {
-            var errorMessage = errorDictionary.MORE_THAN_TWO_INGREDIENTS;
-            expect(function () {createRecipeFrom(ingredients)}).to.throw(errorMessage);
-        });
-    });
+            var errorMessage = errorDictionary.MORE_THAN_TWO_INGREDIENTS
+            si.act({role: 'recipe', cmd: 'generate', ingredients: ingredients}, function (err, result) {
+                expect(err).to.eql(errorMessage)
+            })
+        })
+    })
 
     describe('when I have inputted less than two ingredients', function () {
-        var ingredients = ['apple'];
+        var ingredients = ['apple']
 
         it('should return an error', function () {
-            var errorMessage = errorDictionary.LESS_THAN_TWO_INGREDIENTS;
-            expect(function () {createRecipeFrom(ingredients)}).to.throw(errorMessage);
-        });
-    });
+            var errorMessage = errorDictionary.LESS_THAN_TWO_INGREDIENTS
+            si.act({role: 'recipe', cmd: 'generate', ingredients: ingredients}, function (err, result) {
+                expect(err).to.eql(errorMessage)
+            })
+        })
+    })
 
     describe('when I do not pass in an array', function () {
-        var ingredients = 'apples and bananas';
+        var ingredients = 'apples and bananas'
 
         it('should return an error', function () {
-            var errorMessage = errorDictionary.NOT_AN_ARRAY;
-            expect(function () {createRecipeFrom(ingredients)}).to.throw(errorMessage);
-        });
-    });
+            var errorMessage = errorDictionary.NOT_AN_ARRAY
+            si.act({role: 'recipe', cmd: 'generate', ingredients: ingredients}, function (err, result) {
+                expect(err).to.eql(errorMessage)
+            })
+        })
+    })
 
     describe('when I pass in valid parameters', function () {
-        var ingredients;
-        var result;
+        var ingredients
 
         beforeEach(function () {
-            ingredients = ['apples', 'bananas'];
-            result = createRecipeFrom(ingredients);
-        });
+            ingredients = ['apples', 'bananas']
+        })
 
         it('should return a recipe string', function () {
-            expect(result).to.be.a('string');
-        });
+            si.act({role: 'recipe', cmd: 'generate', ingredients: ingredients}, function (err, result) {
+                expect(result).to.be.a('string')
+            })
+        })
 
         it('should include the ingredients in the recipe', function () {
-            expect(result).to.contain(ingredients[0]);
-            expect(result).to.contain(ingredients[1]);
-        });
+            si.act({role: 'recipe', cmd: 'generate', ingredients: ingredients}, function (err, result) {
+                expect(result).to.contain(ingredients[0])
+                expect(result).to.contain(ingredients[1])
+            })
+        })
 
         it('should include the starting phrase', function () {
-            expect(result).to.contain(phraseDictionary.firstPhrase);
-        });
+            si.act({role: 'recipe', cmd: 'generate', ingredients: ingredients}, function (err, result) {
+                expect(result).to.contain(phraseDictionary.firstPhrase)
+            })
+        })
 
         it('should include the ending phrase', function () {
-            expect(result).to.contain(phraseDictionary.lastPhrase);
-        });
-    });
-});
+            si.act({role: 'recipe', cmd: 'generate', ingredients: ingredients}, function (err, result) {
+                expect(result).to.contain(phraseDictionary.lastPhrase)
+            })
+        })
+    })
+})
